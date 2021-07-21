e77cd1bfc098231dfcfcb565dd7410fbf940197e
Kamil Kowalewski <szamil24@gmail.com>
2021-07-21 17:35:51 +0200

    Add more from previous project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # amazon-offer-verifier
+
+#### Install Dependencies
+In main directory run
+```
+pip install -r requirements.txt
+```
+
+#### Functional Requirements
+* Access to Internet is required
+
+#### How to use
+* Run `python main.py -h` in order to get information about parameters
+* Run `python main.py -p "OFFER_TITLE" --pdf -s`
+* Additional parameter `--pdf` generates pdf report
+* Additional parameter `-s` generates clustering statistics 
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import subprocess
 import sys
+from argparse import ArgumentParser, Namespace
+
+from dotenv import load_dotenv
 
 """
 """
 
-
-# VAR ------------------------------------------------------------------------ #
 
 # MAIN ----------------------------------------------------------------------- #
 def main() -> None:
-    display_finish()
+    load_dotenv()
+    args = prepare_args()
+    search_phrase: str = args.phrase
+    generate_pdf: bool = args.pdf
+    generate_statistics: bool = args.statistics
+
+    # user_interface: UserInterface = UserInterface(search_phrase, generate_pdf, generate_statistics)
+    # user_interface.display_result()
 
 
 # DEF ------------------------------------------------------------------------ #
+def prepare_args() -> Namespace:
+    arg_parser = ArgumentParser()
+
+    arg_parser.add_argument(
+        "-p", "--phrase", required=True, type=str, help="Search phrase"
+    )
+    arg_parser.add_argument(
+        "--pdf", default=False, action="store_true", help="Generate PDF report"
+    )
+    arg_parser.add_argument(
+        "-s", "--statistics", default=False, action="store_true",
+        help="Generate clustering statistics"
+    )
+
+    return arg_parser.parse_args()
+
 
 # UTIL ----------------------------------------------------------------------- #
 def check_types_check_style() -> None:
@@ -28,17 +52,11 @@ def check_if_exists_in_args(arg: str) -> bool:
     return arg in sys.argv
 
 
-def display_finish() -> None:
-    print("------------------------------------------------------------------------")
-    print("FINISHED")
-    print("------------------------------------------------------------------------")
-
-
 # __MAIN__ ------------------------------------------------------------------- #
 if __name__ == "__main__":
-    if check_if_exists_in_args("-t"):
+    if check_if_exists_in_args("--type"):
         check_types_check_style()
-    elif check_if_exists_in_args("-b"):
+    elif check_if_exists_in_args("--build"):
         compile_to_pyc()
     else:
         main()
