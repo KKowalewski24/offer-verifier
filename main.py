0b5d76dab843e7a25d5a64d90d8ab518c3806e32
Kamil Kowalewski <szamil24@gmail.com>
2021-07-21 17:29:49 +0200

    Initial commit of project

---
+++ main.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+
+"""
+"""
+
+
+# VAR ------------------------------------------------------------------------ #
+
+# MAIN ----------------------------------------------------------------------- #
+def main() -> None:
+    display_finish()
+
+
+# DEF ------------------------------------------------------------------------ #
+
+# UTIL ----------------------------------------------------------------------- #
+def check_types_check_style() -> None:
+    subprocess.call(["mypy", "."])
+    subprocess.call(["flake8", "."])
+
+
+def compile_to_pyc() -> None:
+    subprocess.call(["python", "-m", "compileall", "."])
+
+
+def check_if_exists_in_args(arg: str) -> bool:
+    return arg in sys.argv
+
+
+def display_finish() -> None:
+    print("------------------------------------------------------------------------")
+    print("FINISHED")
+    print("------------------------------------------------------------------------")
+
+
+# __MAIN__ ------------------------------------------------------------------- #
+if __name__ == "__main__":
+    if check_if_exists_in_args("-t"):
+        check_types_check_style()
+    elif check_if_exists_in_args("-b"):
+        compile_to_pyc()
+    else:
+        main()
