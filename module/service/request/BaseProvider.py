32e171a64fdcaa6b721780dee2ffac81ceaa4e6d
Kamil Kowalewski <szamil24@gmail.com>
2021-08-01 16:53:58 +0200

    Improve making requests

--- module/service/request/BaseProvider.py
+++ module/service/request/BaseProvider.py
@@ -4,6 +4,8 @@ from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
+from requests import Response
+from requests.exceptions import ChunkedEncodingError
 
 from module.constants import ERROR_PAGE_PHRASE, HTML_PARSER, REQUEST_HEADER
 from module.service.Logger import Logger
@@ -23,11 +25,26 @@ class BaseProvider(ABC):
 
 
     def get_beautiful_soup_instance(self, url: str) -> Tuple[BeautifulSoup, bool]:
-        response = self.requests_session.get(url)
+        response = self._make_request(url)
         self.logger.info("url: " + url + " ||| Status Code: " + str(response.status_code))
         soup: BeautifulSoup = BeautifulSoup(response.content, HTML_PARSER)
         is_error_page: bool = ERROR_PAGE_PHRASE in soup.title.string
         return soup, is_error_page
+
+
+    def _make_request(self, url: str) -> Response:
+        is_request_exception = True
+        response = None
+
+        while is_request_exception:
+            try:
+                response = self._create_session().get(url)
+                is_request_exception = False
+            except ChunkedEncodingError as e:
+                print(is_request_exception)
+                self.logger.error("ChunkedEncodingError " + str(e))
+
+        return response
 
 
     def urlparse_path_replace(self, url: str, replaced_text: str, replacing_text: str = "") -> str:
