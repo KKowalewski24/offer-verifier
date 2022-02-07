82a64761bc1afe567d0d827676c072945712a60e
Kamil Kowalewski <szamil24@gmail.com>
2022-02-07 16:57:25 +0100

    Add divide_into_snapshots.py

--- run_experiments.py
+++ run_experiments.py
@@ -9,25 +9,38 @@ from module.service.PdfGenerator import PdfGenerator
 from module.service.clustering.KMeansClusterizer import KMeansClusterizer
 from module.utils import run_main
 
+"""
+"""
+
+# VAR ------------------------------------------------------------------------ #
 EXPERIMENTS_RESULTS_DIR: str = "experiment_results"
-DATASET_DIR: str = "dataset_snapshot/"
+DATASET_DIR: str = "results/"
 
 latex_generator: LatexGenerator = LatexGenerator(EXPERIMENTS_RESULTS_DIR)
 pdf_generator: PdfGenerator = PdfGenerator()
 logger = Logger().get_logging_instance()
 
 
+# MAIN ----------------------------------------------------------------------- #
 def main() -> None:
     args = prepare_args()
 
     dataset_paths = glob.glob(DATASET_DIR + "*" + PICKLE_EXTENSION)
+    arr = []
     for dataset_path in dataset_paths:
         offer_verifier: OfferVerifier = OfferVerifier(
             path_to_local_file=dataset_path, clusterizer=KMeansClusterizer
         )
         combined_offers, statistics = offer_verifier.verify()
+        print(f"Is verified: {combined_offers[0][1]}, offers count: {len(combined_offers[0][0])}")
+        print(f"Is verified: {combined_offers[1][1]}, offers count: {len(combined_offers[1][0])}")
+        print()
+        arr.append([len(combined_offers[0][0]), len(combined_offers[1][0])])
+        pdf_generator.generate(combined_offers)
+    print(arr)
 
 
+# DEF ------------------------------------------------------------------------ #
 # def _display_statistics(statistics: Statistics) -> None:
 #     print("\n\nNumber of offers :", statistics.offers_number)
 #     print("Silhouette score:", statistics.silhouette_score)
@@ -46,5 +59,6 @@ def prepare_args() -> Namespace:
     return arg_parser.parse_args()
 
 
+# __MAIN__ ------------------------------------------------------------------- #
 if __name__ == "__main__":
     run_main(main)
