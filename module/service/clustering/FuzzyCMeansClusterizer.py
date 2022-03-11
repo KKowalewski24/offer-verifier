1044b1e2b94c7f673d1de527eba4f16b7324ceae
Kamil Kowalewski <szamil24@gmail.com>
2022-03-11 17:34:53 +0100

    Remove old datasets

--- module/service/clustering/FuzzyCMeansClusterizer.py
+++ module/service/clustering/FuzzyCMeansClusterizer.py
@@ -5,13 +5,11 @@ import pandas as pd
 
 
 class FuzzyCMeansClusterizer(MeansClusterizer):
-    RANDOM_STATE_VALUE = 21
-
 
     def perform_means_clusterization(self, dataset: pd.DataFrame) -> None:
         # TODO
         # fcm = FCM(**params)
-        # fcm.random_state = FuzzyCMeansClusterizer.RANDOM_STATE_VALUE
+        # fcm.random_state = Clusterizer.RANDOM_STATE
         # fcm.fit(self.X)
         # self.y_pred = fcm.predict(self.X)
         pass
