e9935cf85f31ee3a053615ed431b9c611e8ee864
Kamil Kowalewski <szamil24@gmail.com>
2022-03-16 12:19:13 +0100

    Add rest of impl of FuzzyCMeansClusterizer.py

--- module/service/clustering/FuzzyCMeansClusterizer.py
+++ module/service/clustering/FuzzyCMeansClusterizer.py
@@ -11,5 +11,5 @@ class FuzzyCMeansClusterizer(MeansClusterizer):
     def perform_means_clusterization(self, dataset: pd.DataFrame) -> None:
         fcm = FCM(n_clusters=MeansClusterizer.K_PARAM)
         fcm.random_state = Clusterizer.RANDOM_STATE
-        # fcm.fit(dataset.to_numpy())
-        # self.cluster_labels: np.ndarray = fcm.predict(dataset.to_numpy())
+        fcm.fit(dataset.to_numpy())
+        self.cluster_labels: np.ndarray = fcm.predict(dataset.to_numpy())
