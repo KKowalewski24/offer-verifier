0e4bb30fa73aa1e219b41e838919eb1f524b4519
Kamil Kowalewski <szamil24@gmail.com>
2022-04-02 21:32:45 +0200

    Add more in BenchmarkFeatureExtractor.py

--- module/service/evaluator/benchmark/BenchmarkFeatureExtractor.py
+++ module/service/evaluator/benchmark/BenchmarkFeatureExtractor.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 from typing import List, Tuple
 
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 from module.model.Offer import Offer
+from module.model.ProductReview import ProductReview
 from module.service.evaluator.FeatureExtractor import FeatureExtractor
 
 
@@ -11,9 +17,27 @@ class BenchmarkFeatureExtractor(FeatureExtractor):
         self.dataset: List[Tuple[Offer, float]] = []
 
 
+    def calculate_score(self) -> BenchmarkFeatureExtractor:
+        self._fix_not_valid_reviews()
+
+        for offer in self.offers:
+            credible_reviews: List[ProductReview] = []
+            for review in offer.reviews:
+                if self._is_credible_review(review):
+                    credible_reviews.append(review)
+
+            offer.reviews = credible_reviews
+            score = pd.Series([review.stars_number for review in offer.reviews]).mean()
+            self.dataset.append((offer, score))
+
+        return self
+
+
     def get_dataset(self) -> List[Tuple[Offer, float]]:
         return self.dataset
 
 
-    def _get_sentiment_polarity(self) -> None:
-        pass
+    def _is_credible_review(self, review: ProductReview) -> bool:
+        SentimentIntensityAnalyzer().polarity_scores(self._prepare_text(review.text_content))
+        # TODO
+        return False
