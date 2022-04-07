50197a7699dd4e78fb83265376cbad1e1b6520ba
Kamil Kowalewski <szamil24@gmail.com>
2022-04-07 21:11:48 +0200

    Add parallel to run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -1,22 +1,25 @@
 import glob
 from argparse import ArgumentParser, Namespace
-from typing import List
+from concurrent.futures.process import ProcessPoolExecutor
+from typing import Any, List, Tuple
 
 from module.constants import PICKLE_EXTENSION
 from module.interface.PdfGenerator import PdfGenerator
+from module.model.Offer import Offer
 from module.model.Statistics import Statistics
 from module.service.OfferVerifier import OfferVerifier
+from module.service.common.LatexGenerator import LatexGenerator
+from module.service.common.Logger import Logger
 from module.service.evaluator.benchmark.BenchmarkEvaluator import BenchmarkEvaluator
 from module.service.evaluator.clustering.FuzzyCMeansEvaluator import FuzzyCMeansEvaluator
 from module.service.evaluator.clustering.KMeansEvaluator import KMeansEvaluator
-from module.service.common.LatexGenerator import LatexGenerator
-from module.service.common.Logger import Logger
 from module.utils import run_main
 
 """
 """
 
 # VAR ------------------------------------------------------------------------ #
+ENABLE_PARALLEL: bool = True
 EXPERIMENTS_RESULTS_DIR: str = "experiment_results"
 DATASET_DIR: str = "dataset_snapshot/"
 
@@ -33,22 +36,48 @@ def main() -> None:
     evaluators: List = [KMeansEvaluator, FuzzyCMeansEvaluator, BenchmarkEvaluator]
 
     for dataset_path in dataset_paths:
-        result = []
-        for evaluator in evaluators:
-            offer_verifier: OfferVerifier = OfferVerifier(
-                path_to_local_file=dataset_path, evaluator=evaluator
-            )
-            combined_offers, statistics = offer_verifier.verify()
+        if not ENABLE_PARALLEL:
+            result = []
+            for evaluator in evaluators:
+                offer_verifier: OfferVerifier = OfferVerifier(
+                    path_to_local_file=dataset_path, evaluator=evaluator
+                )
+                combined_offers, statistics = offer_verifier.verify()
 
-            print(f"Is verified: {combined_offers[0][1]}, offers count: {len(combined_offers[0][0])}")
-            print(f"Is verified: {combined_offers[1][1]}, offers count: {len(combined_offers[1][0])}")
-            print()
-            result.append([len(combined_offers[0][0]), len(combined_offers[1][0])])
-            pdf_generator.generate(combined_offers)
-        print(result)
+                _display_result(evaluator.__name__, combined_offers)
+                print()
+                result.append([len(combined_offers[0][0]), len(combined_offers[1][0])])
+            print(result)
+
+        else:
+            evaluators_len = len(evaluators)
+            with ProcessPoolExecutor() as executor:
+                executor.map(
+                    run_parallel,
+                    evaluators,
+                    [dataset_path] * evaluators_len
+                )
 
 
 # DEF ------------------------------------------------------------------------ #
+def run_parallel(evaluator, dataset_path: str) -> Any:
+    offer_verifier: OfferVerifier = OfferVerifier(
+        path_to_local_file=dataset_path, evaluator=evaluator
+    )
+
+    combined_offers, statistics = offer_verifier.verify()
+    _display_result(evaluator.__name__, combined_offers)
+
+
+def _display_result(
+        evaluator_name: str, combined_offers: Tuple[Tuple[List[Offer], bool], Tuple[List[Offer], bool]]
+) -> None:
+    pdf_generator.generate(combined_offers)
+    print(evaluator_name)
+    for combined_offer in combined_offers:
+        print(f"Is verified: {combined_offer[1]}, offers count: {len(combined_offer[0])}")
+
+
 def _display_statistics(statistics: Statistics) -> None:
     pass
 
