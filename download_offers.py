cb4e79a3f6f1096c986f787a830da5bdea99f683
Kamil Kowalewski <szamil24@gmail.com>
2022-05-25 12:04:23 +0200

    Add first offer

--- download_offers.py
+++ download_offers.py
@@ -1,19 +1,40 @@
 from typing import List, Tuple
 
-from module.constants import JSON_EXTENSION, OFFERS_PATH
+from module.constants import EBAY_ITEM_PATH, JSON_EXTENSION, OFFERS_PATH, RESULTS_DIRECTORY
 from module.model.OffersWrapper import OffersWrapper
 from module.service.RequestProvider import RequestProvider
 from module.service.common.Logger import Logger
-from module.utils import display_and_log_info, get_filename, run_main, save_json_to_file
+from module.utils import create_directory, display_and_log_info, get_filename, run_main, save_json_to_file
 
 OFFERS_NAME_IDS: List[Tuple[str, List[str]]] = [
-    ("", [""])
+    (
+        "amd ryzen 9 5950x 16-core",
+        [
+            "393714612880",
+            "353429471457",
+            "115396323255",
+            "255316126188",
+            "324951940577",
+            "324480526970",
+            "265460231242",
+            "324480324523",
+            "114831636773",
+            "164930511413",
+            "402806621472",
+            "255003155940",
+            "174789972980",
+            "185278620718",
+            "115396323255",
+        ]
+    ),
 ]
 
 logger = Logger().get_logging_instance()
 
 
 def main() -> None:
+    create_directory(RESULTS_DIRECTORY)
+
     for offer_name, offer_ids in OFFERS_NAME_IDS:
         display_and_log_info(logger, f"Search phrase: {offer_name}")
         offers = RequestProvider().get_offers_by_ids(offer_ids)
@@ -24,5 +45,13 @@ def main() -> None:
         )
 
 
+def display_url_to_offers() -> None:
+    for offer_name, offer_ids in OFFERS_NAME_IDS:
+        print(f"Offer name: {offer_name}")
+        for offer_id in offer_ids:
+            print(EBAY_ITEM_PATH + offer_id)
+
+
 if __name__ == "__main__":
+    # display_url_to_offers()
     run_main(main)
--- generate_snapshots.py
+++ generate_snapshots.py
@@ -1,12 +1,12 @@
 from argparse import ArgumentParser, Namespace
 from typing import List, Tuple
 
-from module.constants import JSON_EXTENSION, OFFERS_PATH
+from module.constants import JSON_EXTENSION, OFFERS_PATH, RESULTS_DIRECTORY
 from module.model.Offer import Offer
 from module.model.OffersWrapper import OffersWrapper
 from module.service.RequestProvider import RequestProvider
 from module.service.common.Logger import Logger
-from module.utils import get_filename, run_main, save_json_to_file
+from module.utils import create_directory, get_filename, run_main, save_json_to_file
 
 """
 """
@@ -19,6 +19,7 @@ logger = Logger().get_logging_instance()
 def main() -> None:
     logger.info("Start of generate_snapshots.py")
     args = prepare_args()
+    create_directory(RESULTS_DIRECTORY)
 
     offers_id: List[Tuple[str, str]] = [
         ("293882212178", "AMD Ryzen 9 5900X 12-core 24-thread Desktop Processor - 12 cores And 24 threads")
